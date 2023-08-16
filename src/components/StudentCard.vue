97eb1b4e6fabb4e044476fc52291c88b5f2fa591
MM <phacharanan_th@cmu.ac.th>
2023-08-17 01:53:44 +0700

    add photo of student

--- src/components/StudentCard.vue
+++ src/components/StudentCard.vue
@@ -15,8 +15,8 @@ const props = defineProps({
     <div class="student-class flex flex-col items-center justify-center ">
         <div class="grid gap-5 grid-cols-2 p-5 w-80 h-[130px] cursor-pointer
         border border-gray-700 rounded-lg mb-4 transition-transform duration-300 transform hover:scale-105 hover:shadow-md hover:bg-blue-100">
-            
-            <img src="https://static.vecteezy.com/system/resources/previews/000/439/863/original/vector-users-icon.jpg"
+
+            <img :src="student?.profileimage"
                 alt="" class="w-24 h-24 object-cover rounded-full ">
 
             <div class=" ml-4">
