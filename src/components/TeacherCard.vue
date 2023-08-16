a25808b445a2c90a19da4f02f8861eda3d379c08
MM <phacharanan_th@cmu.ac.th>
2023-08-17 02:08:37 +0700

    add photo of teacher

--- src/components/TeacherCard.vue
+++ src/components/TeacherCard.vue
@@ -16,7 +16,7 @@ const props = defineProps({
         <div class="grid gap-5 grid-cols-2 p-5 w-80 h-[130px] cursor-pointer
         border border-gray-700 rounded-lg mb-4 transition-transform duration-300 transform hover:scale-105 hover:shadow-md hover:bg-blue-100">
 
-            <img src="https://static.vecteezy.com/system/resources/previews/000/439/863/original/vector-users-icon.jpg"
+            <img :src="teacher?.profileimage"
                 alt="" class="w-24 h-24 object-cover rounded-full ">
 
             <div class="ml-4">
