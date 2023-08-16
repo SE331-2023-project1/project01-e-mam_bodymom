79d3fc69a15ed47fc0de3a73950334d8bf3e477d
Thiwakon Sakunchao <thiwakon_sakunchao@cmu.ac.th>
2023-08-16 17:26:37 +0700

    create studentlist and studentcard

---
+++ src/components/StudentCard.vue
@@ -0,0 +1,54 @@
+<script setup lang="ts">
+import type { StudentItem } from '@/type';
+import type { PropType } from 'vue';
+
+const props = defineProps({
+    student: {
+        type: Object as PropType<StudentItem>
+        , require: true
+    }
+})
+</script>
+
+
+<template>
+    <div class="student-class">
+        <div class="student-card">
+
+            <span> {{ student?.name }} {{ student?.surname }}</span>
+            <ul>
+                <li v-for="course in student.courselist" :key="course">{{ course }}</li>
+            </ul>
+
+            <img src="https://static.vecteezy.com/system/resources/previews/000/439/863/original/vector-users-icon.jpg"
+                alt="">
+        </div> 
+    </div>
+</template>
+
+<style scoped>
+.student-card {
+    padding: 20px;
+    width: 250px;
+    cursor: pointer;
+    border: 1px solid #39495c;
+    margin-bottom: 18px;
+}
+
+.student-card:hover {
+    transform: scale(1.01);
+    box-shadow: 0 3px 12px 0 rgb(0, 0, 0, 0.2);
+}
+
+.student-link {
+    color: #2c3e50;
+    text-decoration: none;
+}
+
+.student-card img {
+    width: 100px;
+    height: 100px;
+    object-fit: cover;
+    border-radius: 50%;
+}
+</style>
