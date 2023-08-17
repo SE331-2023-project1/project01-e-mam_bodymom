55b3d6edc73f87de88653a2ece20d2effcd55bf7
MM <phacharanan_th@cmu.ac.th>
2023-08-17 23:04:11 +0700

    edit tailwind in TeacherCard

--- src/components/TeacherCard.vue
+++ src/components/TeacherCard.vue
@@ -15,7 +15,8 @@ const props = defineProps({
     <div class="teacher-class flex flex-col items-center justify-center ">
         <div
             class="grid gap-5 grid-cols-2 p-5 w-80 h-[130px] cursor-pointer
-        border border-gray-700 rounded-lg mb-4 transition-transform duration-300 transform hover:scale-105 hover:shadow-md hover:bg-blue-100">
+        border border-gray-700 rounded-lg mb-4 transition-transform duration-300 transform hover:scale-105 
+        hover:shadow-md hover:bg-gray-100 bg-white">
 
             <div class="flex justify-center items-center">
                 <img :src="teacher?.profileimage" alt="" class="w-24 h-24 object-cover rounded-full ">
