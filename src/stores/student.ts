9937ae908f4dc8572287c305e962dc5d6dd0328d
Thanakorn Waleejaroenpong <tanakorn_wal@cmu.ac.th>
2023-08-19 15:56:11 +0700

    prepare getter to use

--- src/stores/student.ts
+++ src/stores/student.ts
@@ -6,6 +6,12 @@ export const useStudentStore = defineStore('student', {
     state: () => ({
         students: null as StudentItem | null
     }),
+    getters: {
+        getStudent: (state) => state.students,
+        getStudentById: (state) => (id: number) => {
+            return state.students.find((student: {id: number} ) => student.id === id) || null
+        }
+    },
     actions: {
         setStudent(students: StudentItem) {
             this.students = students
--- src/stores/teacher.ts
+++ src/stores/teacher.ts
@@ -1,11 +1,16 @@
 import type { TeacherItem } from "@/type";
 import { defineStore } from "pinia";
 
-
 export const useTeacherStore = defineStore('teacher', {
     state: () => ({
         teachers: null as TeacherItem | null
     }),
+    getters: {
+        getTeacher: (state) => state.teachers,
+        getTeacherById: (state) => (id: string) => {
+            return state.teachers.find((teacher: { id: string; }) => teacher.id === id) || null
+        }
+    },
     actions: {
         setTeacher(teachers: TeacherItem) {
             this.teachers = teachers
