854546db8f52d8f9a511e510928ed322cbfd32f8
MM <phacharanan_th@cmu.ac.th>
2023-08-21 03:32:03 +0700

    styling notfound page

--- src/views/NotFoundView.vue
+++ src/views/NotFoundView.vue
@@ -11,10 +11,14 @@ defineProps({
 </script>
 
 <template>
-    <div class="student-class flex flex-col items-center justify-center text-black">
-        <h1>Oop!</h1>
-        <h3>The {{ resource }} you're looking for is not here</h3>
-        <RouterLink :to="{name: 'home-page'}">Back to home page</RouterLink>
+    <div class="flex flex-col items-center justify-center my-5">
+        <img src="../assets/404-error.png" class="notfound">
+        <h1 class="text-3xl  font-bold font-fig">Oop!</h1>
+        <h3 class="text-2xl  text-center font-bold font-fig">The {{ resource }} you're looking for is not here</h3>
+        <RouterLink :to="{name: 'home-page'}" class="mt-3">
+            <button class="text-white mt-2 bg-blue-500 hover:bg-blue-700 font-fig py-1 px-4 shadow-md rounded-lg">
+                Back to home page</button>
+            </RouterLink>
     </div>
     
 </template>
