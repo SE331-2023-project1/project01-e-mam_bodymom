0543b88155ce22325a1aa3b9fe637d691801ff7b
MM <phacharanan_th@cmu.ac.th>
2023-08-21 04:02:37 +0700

    adjust hompage responsiveness

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -4,8 +4,8 @@ import TheWelcome from '../components/TheWelcome.vue'
 
 <template>
   <main class="flex flex-col items-center justify-center my-5 text-black">
-    <h1 class="font-fig text-3xl font-bold  px-4 py-2">SE331 TERM PROJECT I</h1>
-    <p class="font-mono">Component-Based Software Development</p>
+    <h1 class="font-fig text-3xl font-bold  px-4 py-2 text-center">SE331 TERM PROJECT I</h1>
+    <p class="font-mono text-center">Component-Based Software Development</p>
     <br>
     <p class="font-fig font-bold text-base ">Group Name: E-Mam_BodyMom</p>
     <h2 class="font-fig my-2 font-bold">Team Members</h2>
