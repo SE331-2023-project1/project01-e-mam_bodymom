3d0b61f15c4b3e420cefcf487cb520ade220209e
MM <phacharanan_th@cmu.ac.th>
2023-10-19 16:44:25 +0700

    add flashmessage
    - login
    - register

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -1,42 +1,55 @@
 <template>
-    <div class="flex min-h-full flex-1 flex-col justify-center-px-6 py-12 lg:px-8">
-        <div class="sm:mx-auto sm:w-full sm:max-w-sm">
-            <img class="mx-auto h-[160px] w-auto" src="../assets/bodymoms.png" alt="Your Company">
-            <h2 class="mt-4 text-center text-lg font-bold leading-9 tracking-tight text-gray-900">Welcome to SE331 Project - BodyMom</h2>
-            <h2 class="mt-1 text-center text-lg font-bold leading-9 tracking-tight text-blue-700">Log in to your account</h2>
-        </div>
-        <div class="mt-3 sm:mx-auto sm:w-full sm:max-w-sm">
-            <form class="space-y-6" action="#" @submit.prevent="onSubmit">
-                <div>
-                    <label for="email" class="block text-sm font-medium leading-6 text-gray-900">Email Address</label>
-                    <InputText type="text" v-model="email" class="text-emerald-600 text-sm font-semibold" placeholder="example@example.com"></InputText>
-                    <div v-if="errors['email']" class="text-red-500 text-sm my-2" style="font-weight: 600; font-size: small;">❌ {{ errors['email'] }}</div>
-                </div>
-                <div>
-                    <div class="flex items-center justify-between">
-                        <label for="password" class="block text-sm font-medium leading-6 text-gray-900">Password</label>
-                        <div class="text-sm">
-                            <a href="#" class="font-semibold text-indigo-600 hover:text-indigo-500">Forgot password</a>
-                        </div>
-                    </div>
-                    <div class="mt-2">
-                        <InputText v-model="password" type="password" class="text-emerald-600 text-sm font-semibold" placeholder="Enter Password (at least 6 characters long)" ></InputText>
-                        <div v-if="errors['password']" class="text-red-500 text-sm my-2" style="font-weight: 600; font-size: small;">❌ {{ errors['password'] }}</div>
-
-                    </div>
-                </div>
-                <div>
-                    <button type="submit" class="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm
-                    hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Log in</button>
-                </div>
-            </form>
-            <p class="mt-10 text-center text-sm text-gray-500">
-                Doesn't have an account yet?
-                {{ ' ' }}
-               <RouterLink :to="{ name: 'register-page' }" class="font-semibold leading-6 text-indigo-600 hover:text-indigo-400">Try to register here</RouterLink>
-            </p>
-        </div>
+  <div id="flashMessage" class="mb-2 animate-pulse text-center text-base font-fig bg-green-500 font-fig text-white"
+    v-if="message">
+    <h4>{{ message }}</h4>
+  </div>
+  <FlashMessage />
+  <div class="flex min-h-full flex-1 flex-col justify-center-px-6 py-12 lg:px-8">
+    <div class="sm:mx-auto sm:w-full sm:max-w-sm">
+      <img class="mx-auto h-[160px] w-auto" src="../assets/bodymoms.png" alt="Your Company">
+      <h2 class="mt-4 text-center text-lg font-bold leading-9 tracking-tight text-gray-900">Welcome to SE331 Project -
+        BodyMom</h2>
+      <h2 class="mt-1 text-center text-lg font-bold leading-9 tracking-tight text-blue-700">Log in to your account</h2>
     </div>
+    <div class="mt-3 sm:mx-auto sm:w-full sm:max-w-sm">
+      <form class="space-y-6" action="#" @submit.prevent="onSubmit">
+        <div>
+          <label for="email" class="block text-sm font-medium leading-6 text-gray-900">Email Address</label>
+          <InputText type="text" v-model="email" class="text-emerald-600 text-sm font-semibold"
+            placeholder="example@example.com"></InputText>
+          <div v-if="errors['email']" class="text-red-500 text-sm my-2" style="font-weight: 600; font-size: small;">❌ {{
+            errors['email'] }}</div>
+        </div>
+        <div>
+          <div class="flex items-center justify-between">
+            <label for="password" class="block text-sm font-medium leading-6 text-gray-900">Password</label>
+            <div class="text-sm">
+              <a href="#" class="font-semibold text-indigo-600 hover:text-indigo-500">Forgot password</a>
+            </div>
+          </div>
+          <div class="mt-2">
+            <InputText v-model="password" type="password" class="text-emerald-600 text-sm font-semibold"
+              placeholder="Enter Password (at least 6 characters long)"></InputText>
+            <div v-if="errors['password']" class="text-red-500 text-sm my-2" style="font-weight: 600; font-size: small;">❌
+              {{ errors['password'] }}</div>
+
+          </div>
+        </div>
+        <div>
+          <button type="submit"
+            class="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm
+                    hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Log
+            in</button>
+        </div>
+      </form>
+      <p class="mt-10 text-center text-sm text-gray-500">
+        Doesn't have an account yet?
+        {{ ' ' }}
+        <RouterLink :to="{ name: 'register-page' }" class="font-semibold leading-6 text-indigo-600 hover:text-indigo-400">
+          Try to register here</RouterLink>
+      </p>
+    </div>
+  </div>
 </template>
 
 <script setup lang="ts">
@@ -46,8 +59,10 @@ import { useField, useForm } from 'vee-validate'
 import { useAuthStore } from '@/stores/auth'
 import { useRouter } from 'vue-router'
 import { useMessageStore } from '@/stores/message'
+import { storeToRefs } from 'pinia'
 
-const messageStore = useMessageStore()
+const storeMessage = useMessageStore()
+const { message } = storeToRefs(storeMessage)
 
 const router = useRouter()
 
@@ -55,13 +70,13 @@ const authStore = useAuthStore()
 
 const validationSchema = yup.object({
   email: yup.string()
-//   .required('Please enter your email address.')
-//   .matches(/^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}$/, 'Please enter a valid email address ending with example.com'),
-,
+  //   .required('Please enter your email address.')
+  //   .matches(/^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}$/, 'Please enter a valid email address ending with example.com'),
+  ,
   password: yup
     .string()
-    // .required('Please enter your password.')
-    // .min(6, 'Password must be at least 6 characters long.'), // Minimum 7 characters required
+  // .required('Please enter your password.')
+  // .min(6, 'Password must be at least 6 characters long.'), // Minimum 7 characters required
 })
 
 const { errors, handleSubmit } = useForm({
@@ -77,15 +92,23 @@ const { value: email } = useField<string>('email')
 const { value: password } = useField<string>('password')
 const onSubmit = handleSubmit((values) => {
   authStore.login(values.email, values.password)
-  .then(() => {
-    console.log('login success')
-    router.push({ name: 'home-page' })
-  }).catch((err) => {
-    console.log('error', err)
-    messageStore.updateMessage('could not login')
-    setTimeout(() => {
-        messageStore.resetMessage()
-    }, 3000)
-  })
+    .then(() => {
+      console.log('login success')
+      storeMessage.updateMessage('Login successful');
+      setTimeout(() => {
+        storeMessage.resetMessage()
+      }, 1000)
+      setTimeout(() => {
+        
+        router.push({ name: 'home-page' })
+      }, 1000)
+      
+    }).catch((err) => {
+      console.log('error', err)
+      storeMessage.updateMessage('could not login')
+      setTimeout(() => {
+        storeMessage.resetMessage()
+      }, 3000)
+    })
 })
 </script>
--- src/views/RegisterView.vue
+++ src/views/RegisterView.vue
@@ -9,30 +9,30 @@ import { storeToRefs } from 'pinia'
 
 const authStore = useAuthStore()
 const router = useRouter()
-const messageStore = useMessageStore()
+const storeMessage = useMessageStore()
 
-const { message } = storeToRefs(messageStore)
+const { message } = storeToRefs(storeMessage)
 
 const validationSchema = yup.object({
   username: yup.string()
-  .required('The username is required')
-  .matches(/^[A-Za-z0-9]+$/, 'Username should contain only alphabetic characters and numbers'),
+    .required('The username is required')
+    .matches(/^[A-Za-z0-9]+$/, 'Username should contain only alphabetic characters and numbers'),
 
   firstName: yup.string()
-  .required('The firstName is required')
-  .matches(/^[A-Za-z]+$/, 'First name should contain only alphabetic characters'),
-  
+    .required('The firstName is required')
+    .matches(/^[A-Za-z]+$/, 'First name should contain only alphabetic characters'),
+
   lastName: yup.string()
-  .required('The lastName is required')
-  .matches(/^[A-Za-z]+$/, 'Last name should contain only alphabetic characters'),
+    .required('The lastName is required')
+    .matches(/^[A-Za-z]+$/, 'Last name should contain only alphabetic characters'),
 
   email: yup.string()
-  .required('The email is required')
-  .matches(/^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}$/, 'Please enter a valid email address ending with example.com'),
+    .required('The email is required')
+    .matches(/^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}$/, 'Please enter a valid email address ending with example.com'),
 
   password: yup.string()
-  .required('The password is required')
-  .min(6, 'Password must be at least 6 characters long.'), 
+    .required('The password is required')
+    .min(6, 'Password must be at least 6 characters long.'),
 })
 
 const { errors, handleSubmit } = useForm({
@@ -62,12 +62,17 @@ const onSubmit = handleSubmit((values) => {
     .studentRegister(values.username, values.firstName, values.lastName, values.email, values.password)
     .then(() => {
       router.push({ name: 'event-list' })
+      storeMessage.updateMessage('Registration successful');
+      setTimeout(() => {
+        storeMessage.resetMessage()
+      }, 4000)
+
     })
     .catch(() => {
-      messageStore.updateMessage('could not login')
+      storeMessage.updateMessage('could not register')
 
       setTimeout(() => {
-        messageStore.resetMessage()
+        storeMessage.resetMessage()
       }, 3000)
     })
 })
@@ -77,11 +82,7 @@ const onSubmit = handleSubmit((values) => {
 <template>
   <div class="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
     <div class="sm:mx-auto sm:w-full sm:max-w-sm">
-      <img
-        class="mx-auto h-[160px] w-auto"
-        src="../assets/bodymoms.png"
-        alt=""
-      />
+      <img class="mx-auto h-[160px] w-auto" src="../assets/bodymoms.png" alt="" />
 
       <h2 class="mt-3 text-center text-2xl font-bold leading-9 tracking-tight text-blue-700">
         Register to your account
@@ -94,58 +95,60 @@ const onSubmit = handleSubmit((values) => {
     <div class="mt-3 sm:mx-auto sm:w-full sm:max-w-sm">
       <form class="space-y-6" @submit.prevent="onSubmit">
         <div>
-          <label for="username" class="block text-sm font-medium leading-6 text-gray-900"
-            >Username</label
-          >
-          <InputText type="text" v-model="username" class="text-emerald-600 text-sm font-semibold" placeholder="Username"></InputText>
-          <div v-if="errors['username']" class="text-red-500 text-sm my-2" style="font-weight: 600; font-size: small;">❌ {{ errors['username'] }}</div>
+          <label for="username" class="block text-sm font-medium leading-6 text-gray-900">Username</label>
+          <InputText type="text" v-model="username" class="text-emerald-600 text-sm font-semibold" placeholder="Username">
+          </InputText>
+          <div v-if="errors['username']" class="text-red-500 text-sm my-2" style="font-weight: 600; font-size: small;">❌
+            {{ errors['username'] }}</div>
         </div>
 
         <div>
-          <label for="firstName" class="block text-sm font-medium leading-6 text-gray-900"
-            >First Name</label
-          >
-          <InputText type="text" v-model="firstName" class="text-emerald-600 text-sm font-semibold" placeholder="Enter your first name"></InputText>
-          <div v-if="errors['firstName']" class="text-red-500 text-sm my-2" style="font-weight: 600; font-size: small;">❌ {{ errors['firstName'] }}</div>
-
-        </div>
-
-        <div>
-          <label for="lastName" class="block text-sm font-medium leading-6 text-gray-900"
-            >Last Name</label
-          >
-          <InputText type="text" v-model="lastName" class="text-emerald-600 text-sm font-semibold" placeholder="Enter your last name"></InputText>
-          <div v-if="errors['lastName']" class="text-red-500 text-sm my-2" style="font-weight: 600; font-size: small;">❌ {{ errors['lastName'] }}</div>
+          <label for="firstName" class="block text-sm font-medium leading-6 text-gray-900">First Name</label>
+          <InputText type="text" v-model="firstName" class="text-emerald-600 text-sm font-semibold"
+            placeholder="Enter your first name"></InputText>
+          <div v-if="errors['firstName']" class="text-red-500 text-sm my-2" style="font-weight: 600; font-size: small;">❌
+            {{ errors['firstName'] }}</div>
 
         </div>
 
         <div>
-          <label for="email" class="block text-sm font-medium leading-6 text-gray-900"
-            >Email Address</label
-          >
-          <InputText type="text" v-model="email" class="text-emerald-600 text-sm font-semibold" placeholder="Enter your email address"></InputText>
-          <div v-if="errors['email']" class="text-red-500 text-sm my-2" style="font-weight: 600; font-size: small;">❌ {{ errors['email'] }}</div>
+          <label for="lastName" class="block text-sm font-medium leading-6 text-gray-900">Last Name</label>
+          <InputText type="text" v-model="lastName" class="text-emerald-600 text-sm font-semibold"
+            placeholder="Enter your last name"></InputText>
+          <div v-if="errors['lastName']" class="text-red-500 text-sm my-2" style="font-weight: 600; font-size: small;">❌
+            {{ errors['lastName'] }}</div>
+
+        </div>
+
+        <div>
+          <label for="email" class="block text-sm font-medium leading-6 text-gray-900">Email Address</label>
+          <InputText type="text" v-model="email" class="text-emerald-600 text-sm font-semibold"
+            placeholder="Enter your email address"></InputText>
+          <div v-if="errors['email']" class="text-red-500 text-sm my-2" style="font-weight: 600; font-size: small;">❌ {{
+            errors['email'] }}</div>
 
         </div>
 
         <div>
           <div class="flex items-center justify-start">
-            <label for="password" class="block text-sm font-medium leading-6 text-gray-900"
-              >Password</label
-            >
+            <label for="password" class="block text-sm font-medium leading-6 text-gray-900">Password</label>
           </div>
 
-            <InputText v-model="password" type="password" class="text-emerald-600 text-sm font-semibold" placeholder="Create password"></InputText>
-            <div v-if="errors['password']" class="text-red-500 text-sm my-2" style="font-weight: 600; font-size: small;">❌ {{ errors['password'] }}</div>        </div>
+          <InputText v-model="password" type="password" class="text-emerald-600 text-sm font-semibold"
+            placeholder="Create password"></InputText>
+          <div v-if="errors['password']" class="text-red-500 text-sm my-2" style="font-weight: 600; font-size: small;">❌
+            {{ errors['password'] }}</div>
+        </div>
 
         <div>
-          <button type="submit" class="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm
+          <button type="submit"
+            class="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm
                     hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">
             Register
           </button>
-          
+
         </div>
-        
+
       </form>
 
       <p class="mt-10 text-center text-sm text-gray-500">
@@ -153,11 +156,8 @@ const onSubmit = handleSubmit((values) => {
 
         {{ ' ' }}
 
-        <RouterLink
-          :to="{ name: 'Login' }"
-          class="font-semibold leading-6 text-indigo-600 hover:text-indigo-400"
-          >Log in</RouterLink
-        >
+        <RouterLink :to="{ name: 'Login' }" class="font-semibold leading-6 text-indigo-600 hover:text-indigo-400">Log in
+        </RouterLink>
       </p>
     </div>
   </div>
