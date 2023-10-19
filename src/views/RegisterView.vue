bdc526d6ad6934948764321b643f01febd44fb99
MM <phacharanan_th@cmu.ac.th>
2023-10-19 17:13:16 +0700

    edit flashmessage

--- src/views/RegisterView.vue
+++ src/views/RegisterView.vue
@@ -62,10 +62,10 @@ const onSubmit = handleSubmit((values) => {
     .studentRegister(values.username, values.firstName, values.lastName, values.email, values.password)
     .then(() => {
       router.push({ name: 'event-list' })
-      storeMessage.updateMessage('Registration successful');
-      setTimeout(() => {
-        storeMessage.resetMessage()
-      }, 4000)
+      // storeMessage.updateMessage('Registration successful');
+      // setTimeout(() => {
+      //   storeMessage.resetMessage()
+      // }, 4000)
 
     })
     .catch(() => {
@@ -87,9 +87,10 @@ const onSubmit = handleSubmit((values) => {
       <h2 class="mt-3 text-center text-2xl font-bold leading-9 tracking-tight text-blue-700">
         Register to your account
       </h2>
-      <div class="animate-flashMessage mb-4" v-if="message">
+      <!-- <div class="animate-flashMessage mb-4" v-if="message">
         <h4>{{ message }}</h4>
-      </div>
+      </div> -->
+
     </div>
 
     <div class="mt-3 sm:mx-auto sm:w-full sm:max-w-sm">
@@ -141,6 +142,10 @@ const onSubmit = handleSubmit((values) => {
         </div>
 
         <div>
+          <div id="flashMessage" class="mb-2 animate-pulse text-center text-base font-fig bg-red-500 font-fig text-white"
+            v-if="message">
+            <h4>{{ message }}</h4>
+          </div>
           <button type="submit"
             class="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm
                     hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">
