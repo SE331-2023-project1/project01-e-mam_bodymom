e3027f95db2cfaf8702053defbb57585646b5144
MM <phacharanan_th@cmu.ac.th>
2023-10-20 22:29:37 +0700

    prepared announcement page - draft

--- src/views/announcement/AnnouncementView.vue
+++ src/views/announcement/AnnouncementView.vue
@@ -1,63 +1,9 @@
-<script>
-export default {
-    data() {
-        return {
-            photos: [
-                'https://cdn.britannica.com/38/196638-131-7BF02881/Santa-Claus.jpg',
-                'https://hips.hearstapps.com/hmg-prod/images/christmas-facts-650b513919cd9.jpg?crop=1xw:0.8453434844192634xh;center,top&resize=1200:*',
-                'https://cdn.creatureandcoagency.com/uploads/2020/10/Facts-about-Chrismtas-5.jpg',
-                // Add more photo URLs here
-            ],
-            selectedPhoto: 0, // Initially select the first photo
-        };
-    },
-    methods: {
-        showPhoto(index) {
-            this.selectedPhoto = index;
-        },
-        closePhotoPopup() {
-            this.selectedPhoto = null;
-        },
-        nextPhoto() {
-            if (this.selectedPhoto < this.photos.length - 1) {
-                this.selectedPhoto++;
-            } else {
-                this.selectedPhoto = 0; // Loop back to the first photo
-            }
-        },
-        prevPhoto() {
-            if (this.selectedPhoto > 0) {
-                this.selectedPhoto--;
-            } else {
-                this.selectedPhoto = this.photos.length - 1; // Loop to the last photo
-            }
-        },
-    },
-};
-
-// import { type AnnouncementItem } from '@/type'
-// import { computed, ref, watchEffect, type Ref , onMounted } from 'vue';
-// import AnnouncementService from '@/services/AnnouncementService';
-// import NProgress from 'nprogress'
-// import { onBeforeRouteUpdate, useRouter,useRoute  } from 'vue-router'
-// import type { AxiosResponse } from 'axios';
-// import {useAnnouncementStore} from '@/stores/announcement';
-
-// const router = useRouter
-// const store = useStudentStore();
-// const route = useRoute();
-
-// const announcements: Ref<Array<AnnouncementItem>> = ref([])
-// const totalAnnouncement = ref<number>(10)
-</script>
-
 <template>
     <main class="flex flex-col items-center justify-center">
         <div
             class="mt-5 font-fig flex items-center justify-center p-3 w-3/4 sm:w-2/4 h-4/5 text-2xl font-bold text-gray-900">
             Announcement(s)
         </div>
-
 
         <div
             class="mt-5 mb-10 font-fig flex flex-col items-left justify-left p-3 w-3/4 sm:w-2/4 h-auto text-xl font-bold text-gray-900 bg-white border border-gray-300 rounded-lg shadow-md">
@@ -75,15 +21,26 @@ export default {
                 <p class="font-fig font-semibold text-md">Christmas Event !</p>
                 <p class="font-fig text-md text-gray-600">Ho Ho Ho kriๆๆๆๆ</p>
             </div>
-            <!-- Photo slider -->
-            <div class="mt-5 relative">
-                <img :src="photos[selectedPhoto]" class="object-cover w-full h-60 md:h-96 rounded-lg"
-                    alt="Selected Photo" />
+            <!-- Media Preview -->
+            <div class="mt-5 relative" v-if="mediaExists">
+                <template v-if="isImage">
+                    <!-- Image -->
+                    <img :src="mediaSource" class="object-cover w-full h-60 md:h-96 rounded-lg" alt="Selected Media" />
+                </template>
+                <template v-else-if="isGif">
+                    <!-- GIF -->
+                    <img :src="mediaSource" class="object-cover w-full h-60 md:h-96 rounded-lg" alt="Selected Media" />
+                </template>
+                <template v-else-if="isPdf">
+                    <!-- PDF -->
+                    <a :href="mediaSource" target="_blank" class="text-blue-500 underline">View PDF</a>
+                </template>
+                <!-- Navigation for Images and GIFs -->
                 <div
                     class="absolute top-0 left-0 z-30 flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none">
                     <span
                         class="inline-flex items-center justify-center w-10 h-10 rounded-full bg-white/30 dark:bg-gray-800/30 group-hover:bg-white/50 dark:group-hover:bg-gray-800/60 group-focus:ring-4 group-focus:ring-white dark:group-focus:ring-gray-800/70 group-focus:outline-none"
-                        @click="prevPhoto">
+                        @click="prevMedia">
                         <svg class="w-4 h-4 text-white dark:text-gray-800" aria-hidden="true"
                             xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 6 10">
                             <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
@@ -96,7 +53,7 @@ export default {
                     class="absolute top-0 right-0 z-30 flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none">
                     <span
                         class="inline-flex items-center justify-center w-10 h-10 rounded-full bg-white/30 dark:bg-gray-800/30 group-hover:bg-white/50 dark:group-hover:bg-gray-800/60 group-focus:ring-4 group-focus:ring-white dark:group-focus:ring-gray-800/70 group-focus:outline-none"
-                        @click="nextPhoto">
+                        @click="nextMedia">
                         <svg class="w-4 h-4 text-white dark:text-gray-800" aria-hidden="true"
                             xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 6 10">
                             <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
@@ -107,7 +64,65 @@ export default {
                 </div>
             </div>
         </div>
-
     </main>
 </template>
+  
+<script>
+
+export default {
+    data() {
+        return {
+            mediaFiles: [
+                // {
+                //     type: 'image',
+                //     source: 'URL_TO_IMG',
+                // },
+                // {
+                //     type: 'gif',
+                //     source: 'URL_TO_GIF',
+                // },
+                // {
+                //     type: 'pdf',
+                //     source: 'URL_TO_PDF',
+                // },
+                // Add more media files with different types here
+            ],
+            selectedMedia: 0, // Initially select the first media file
+        };
+    },
+    computed: {
+        mediaSource() {
+            return this.mediaFiles[this.selectedMedia].source;
+        },
+        isImage() {
+            return this.mediaFiles[this.selectedMedia].type === 'image';
+        },
+        isGif() {
+            return this.mediaFiles[this.selectedMedia].type === 'gif';
+        },
+        isPdf() {
+            return this.mediaFiles[this.selectedMedia].type === 'pdf';
+        },
+        mediaExists() {
+            return this.mediaFiles.length > 0;
+        },
+    },
+    methods: {
+        prevMedia() {
+            if (this.selectedMedia > 0) {
+                this.selectedMedia--;
+            } else {
+                this.selectedMedia = this.mediaFiles.length - 1; // Loop to the last media file
+            }
+        },
+        nextMedia() {
+            if (this.selectedMedia < this.mediaFiles.length - 1) {
+                this.selectedMedia++;
+            } else {
+                this.selectedMedia = 0; // Loop back to the first media file
+            }
+        },
+    },
+};
+</script>
   
